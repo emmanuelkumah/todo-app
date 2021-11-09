6d2345b141fa7979d7de343aba229058f6563f95
EmmanuelKumah <mailkumah@gmail.com>
2021-11-09 22:01:01 +0000

    Test init adding of todo

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 //select the text input and button
-const inputText = document.getElementById("inputText");
+const todoInput = document.getElementById("todoInput");
 const submitBtn = document.getElementById("submit-btn");
 
 //add event listener to the button
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(inputText.value);
-  //clear input field
-  inputText.value = "";
+  //console.log(todoInput.value);
+  const newTodo = todoInput.value;
+  //call the addTodo and pass in the todo value
+  addTodo(newTodo);
+  todoInput.value = "";
 });
+
+const todoLists = [];
+function addTodo(todo) {
+  //push todo to Lists
+  todoLists.push({ todo });
+  console.log(todoLists);
+}
+//push the todo into an empty array
